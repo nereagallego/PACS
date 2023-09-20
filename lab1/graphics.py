6974d7353e7a9ace5f5174f8c854e2850aabc104
cborjamoreno <cesarborjamoreno@gmail.com>
2023-09-20 20:46:29 +0200

    graph

--- lab1/graphics.py
+++ lab1/graphics.py
@@ -8,9 +8,16 @@ This is a temporary script file.
 import matplotlib.pyplot as plt
 import numpy as np
 
-x_std=[155, 153, 153.5, 154.3, 155.5, 157, 156, 152.7, 153.8, 154.4, 155.9, 155.9, 155.2, 156.1, 154, 154, 152.9, 153.5, 155.6, 155.1]
-x_eig=[0.028, 0.028, 0.027, 0.028, 0.028, 0.027, 0.026, 0.027, 0.027, 0.027, 0.027, 0.027, 0.027, 0.027, 0.026, 0.028, 0.027, 0.027, 0.027, 0.028]
-y=np.arange(20)
-plt.plot(y,x_std,  'go--', linewidth=1, markersize=1)
-plt.plot(y,x_eig, 'ro--', linewidth=1, markersize=1)
+x_std=[]
+x_eig=[3.250, 3.242, 3.238, 3.241, 3.239, 3.239, 3.238, 3.249, 3.241, 3.239, 3.240, 3.241, 3.240, 3.246, 3.239, 3.241, 3.247, 3.238, 3.241, 3.246]
+x_eig_nv=[6.437, 6.428, 6.439, 6.439, 6.432, 6.432, 6.433, 6.440, 6.392, 6.398, 6.393, 6.405, 6.404, 6.400, 6.402, 6.404, 6.413, 6.397, 6.399, 6.431]
+y= np.arange(1,21)
+plt.ylabel('time (s)')
+plt.xlabel('number of experiment')
+plt.plot(y,x_std,  'go', linewidth=1, markersize=1, label='Standard Matrix Multiplication')
+plt.plot(y,x_eig, 'ro', linewidth=1, markersize=1, label='Eigen Math Library Vectorized')
+plt.plot(y,x_eig_nv, 'bo', linewidth=1, markersize=1, label='Eigen Math Library Non Vectorized')
+default_y_ticks = range(len(y))
+plt.xticks(y)
+plt.legend()
 plt.show()
--- lab1/matrixMultEigen.cpp
+++ lab1/matrixMultEigen.cpp
@@ -1,8 +1,6 @@
 #include <iostream>
 #include <Eigen/Dense>
 
-#define EIGEN_DONT_VECTORIZE 1
-
 int main()
 {
     int N = 3000; 
