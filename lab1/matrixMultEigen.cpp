e0c8ef3f71b9d83b924f0dba7cec0176aa6208e0
cborjamoreno <cesarborjamoreno@gmail.com>
2023-09-20 19:47:06 +0200

    eigen fixed

--- lab1/matrixMultEigen.cpp
+++ lab1/matrixMultEigen.cpp
@@ -1,12 +1,15 @@
 #include <iostream>
 #include <Eigen/Dense>
 
-#define EIGEN_DONT_VECTORIZE
+// #define EIGEN_DONT_VECTORIZE 1
 
 int main()
 {
-    int N = 2000; 
+    int N = 5000; 
 
     Eigen::MatrixXd mat = Eigen::MatrixXd::Random(N,N);
+    Eigen::MatrixXd result(N,N);
+    result = mat*mat;
+
     // std::cout << "Here is mat*mat:\n" << mat*mat << std::endl;
 }
