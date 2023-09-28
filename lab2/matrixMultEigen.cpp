b869a08853312abc7aea149bceacb46434faf446
cborjamoreno <cesarborjamoreno@gmail.com>
2023-09-28 18:01:11 +0200

    lab2

--- lab2/matrixMultEigen.cpp
+++ lab2/matrixMultEigen.cpp
@@ -6,17 +6,17 @@
 int main(int argc, char* argv[])
 {
     if(argc < 2){
-        cerr << "Usage ./matrixMultEigen.cpp <N>" << endl;
+        std::cerr << "Usage ./matrixMultEigen.cpp <N>" << std::endl;
     }
 
     clock_t time;
     time = clock();
-    int N = int(argc[1]); 
+    int N = atoi(argv[1]); 
 
     Eigen::MatrixXd mat = Eigen::MatrixXd::Random(N,N);
     Eigen::MatrixXd result(N,N);
     result = mat*mat;
     time = clock() - time;
-    cout << "Seconds: " << fixed << setprecision(4)
-        << ((float)time)/CLOCKS_PER_SEC << endl;
+    std::cout << "Seconds: " << std::fixed << std::setprecision(4)
+        << ((float)time)/CLOCKS_PER_SEC << std::endl;
 }
--- lab2/script.sh
+++ lab2/script.sh
@@ -2,5 +2,5 @@
 
 for i in {1..10}
 do
-    time ./matrixMultEigen
+    time ./matrixMultEigen $1
 done
