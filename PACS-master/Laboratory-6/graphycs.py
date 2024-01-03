bb0a549829a22ec6aacec27a52c9108f6ec978e6
Nerea <801950@unizar.es>
2024-01-03 16:44:11 +0100

    Update graphycs.py

--- PACS-master/Laboratory-6/graphycs.py
+++ PACS-master/Laboratory-6/graphycs.py
@@ -6,20 +6,26 @@ import numpy as np
 x = ['heterogeneous system', 'CPU multicore', 'GPU']
 y_kernel_time = [13.810394, 21.726354, 10.286731]
 y_overall_time = [49.588739, 95.941153, 65.407475]
+y_launch_kernel = [31.621257, 1.196688, 18.444806]
 
 # Make two histograms in the same plot
 
-fig, axs = plt.subplots(1, 2, figsize=(10, 5))  # 1 row, 2 columns
+fig, axs = plt.subplots(1, 2, figsize=(10, 5))  # 1 row, 3 columns
 
 axs[0].set_title('Total kernel time')
 axs[0].set_ylabel('time (s)')
-axs[0].set_xlabel('type of system')
+
 axs[0].bar(x, y_kernel_time)
 
 axs[1].set_title('Overall execution time')
 axs[1].set_ylabel('time (s)')
-axs[1].set_xlabel('type of system')
+
 axs[1].bar(x, y_overall_time)
+
+# axs[2].set_title('Time to launch the kernel')
+# axs[2].set_ylabel('time (s)')
+
+# axs[2].bar(x, y_launch_kernel)
 
 plt.tight_layout()
 plt.show()
