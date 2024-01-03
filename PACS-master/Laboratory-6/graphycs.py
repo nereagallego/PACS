b721ee8a31b63226dc5f8eeeb92fe78da6f40462
Nerea <801950@unizar.es>
2024-01-03 13:00:54 +0100

    Update graphycs.py

--- PACS-master/Laboratory-6/graphycs.py
+++ PACS-master/Laboratory-6/graphycs.py
@@ -3,7 +3,7 @@ import numpy as np
 
 
 
-x = ['heterogeneous system', 'cpu multicore', 'gpu']
+x = ['heterogeneous system', 'CPU multicore', 'GPU']
 y_kernel_time = [13.810394, 21.726354, 10.286731]
 y_overall_time = [49.588739, 95.941153, 65.407475]
 
@@ -11,12 +11,12 @@ y_overall_time = [49.588739, 95.941153, 65.407475]
 
 fig, axs = plt.subplots(1, 2, figsize=(10, 5))  # 1 row, 2 columns
 
-axs[0].set_title('Time of execution of the kernel')
+axs[0].set_title('Total kernel time')
 axs[0].set_ylabel('time (s)')
 axs[0].set_xlabel('type of system')
 axs[0].bar(x, y_kernel_time)
 
-axs[1].set_title('Overall time of execution')
+axs[1].set_title('Overall execution time')
 axs[1].set_ylabel('time (s)')
 axs[1].set_xlabel('type of system')
 axs[1].bar(x, y_overall_time)
